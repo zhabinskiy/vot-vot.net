c4960665ea007729949e0168bedcf3a6a3c7e640
Elijah Straykov <ilya@straykov.ru>
2017-12-17 01:47:49 +0200

    add logos to Footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -11,12 +11,36 @@ const Container = styled.section`
 `;
 
 const Social = styled.div`
-  float: right;
+  text-align: left;
+  margin-top: 30px;
+
+  @media screen and (min-width: 992px) {
+    margin-top: 0px;
+    text-align: right;
+  }
+`;
+
+const Link = styled.a`
+  color: rgba(255, 255, 255, .6);
+
+  &:hover {
+    color: white;
+  }
 `;
 
 const Copyright = styled.div`
-  margin-top: 130px;
+  margin-top: 80px;
   color: rgba(255, 255, 255, .6);
+
+  @media screen and (min-width: 992px) {
+    margin-top: 130px;
+  }
+`;
+
+const Logo = styled.img`
+  padding-right: 8px;
+  position: relative;
+  top: 4px;
 `;
 
 export default () => (
@@ -33,7 +57,7 @@ export default () => (
         </Row>
         <Row>
           <Col xs={12} lg={12}>
-            <Copyright>Наконец-то запустили Гридли и Кодельная</Copyright>
+            <Copyright>Наконец-то запустили <Link href="http://gridly.ru"><Logo src={require('./gridly-logo.svg')} />Гридли</Link> и <Link href="http://codecode.ru"><Logo src={require('./codecode-logo.svg')} />Кодельная</Link></Copyright>
           </Col>
         </Row>
       </Grid>
